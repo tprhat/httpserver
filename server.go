7524b8460d4ce1fb6f07ed86ee0dd3df3aa07033
tprhat <tomislav.prhat@gmail.com>
2024-05-05 00:11:50 +0200

    fixed client disconnect error

--- server.go
+++ server.go
@@ -14,7 +14,7 @@ type player struct {
 
 func main() {
 	deck := buildDeck()
-	log.Print(deck)
+	// log.Print(deck)
 	log.Print(len(deck))
 	counter := 0
 	players := make([]player, 2)
@@ -23,49 +23,52 @@ func main() {
 		log.Fatalf("Error: %v\n", err)
 	}
 	fmt.Printf("Server started: %v\n", ln)
-// defer ln.Close()
-	for {
-		
-		if counter < 2 {
-			conn, err := ln.Accept()
-			// log.Printf("Client %s connected!", conn.RemoteAddr())
-			players[counter] = player{name: counter}
-			if err != nil {
-				log.Printf("Error accepting: %v", err)
-			}
 
+	defer ln.Close()
+	for {
+		conn, err := ln.Accept()
+		if counter >= 2 {
+			log.Printf("Rejecting connection from %s", conn.RemoteAddr())
+			conn.Close()
+			continue
+		}
+		log.Printf("Client %s connected!", conn.RemoteAddr())
+		players[counter] = player{name: counter}
+		if err != nil {
+			log.Printf("Error accepting: %v", err)
+			continue
+		}
+		if conn == nil {
+			log.Print("nil")
+		} else {
 			go HandleClient(conn, &players[counter], &counter)
 			counter++
 			log.Printf("num of players = %d", counter)
 			log.Printf("current players are: %v", players)
-		} else {
-			ln.Close()
 		}
 	}
 }
 
 func HandleClient(conn net.Conn, player *player, counter *int) {
-	// defer conn.Close()
-	log.Print(counter)
-	log.Print(*counter)
+	defer conn.Close()
+	log.Printf("player %d joined:", player.name)
 	buffer := make([]byte, 1024)
 
 	for {
 		n, err := conn.Read(buffer)
 		if err != nil {
 			*counter--
-			conn.Close()
 			if err == io.EOF {
-				log.Printf("connection %d closed", &player.name)
+				log.Printf("connection %d closed", player.name)
 			} else {
-				log.Printf("Error: %v", err)
+				log.Printf("Error: client closed connection. Details: %v", err)
 			}
 			return
 		}
 		log.Printf("From: player #%d\tData: %s\n", player.name, buffer[:n])
-		_, err = conn.Write([]byte("Here is res:"))
-		if err != nil {
-			log.Print("Error sending response")
-		}
+		// _, err = conn.Write([]byte("Here is res:"))
+		// if err != nil {
+		// log.Print("Error sending response")
+		// }
 	}
 }
